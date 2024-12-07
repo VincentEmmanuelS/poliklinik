b4b7397f1d8917202eb8f8e71111d7f175755c5f
VincentEmmanuelS <suwardyvincent@gmail.com>
2024-12-07 15:51:45 +0700

    add navbar

--- poliklinik/src/app/page.js
+++ poliklinik/src/app/page.js
@@ -1,10 +1,42 @@
 import { Button } from "@nextui-org/react";
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   // return <h1>Hello, Next.js!</h1>
   return (
-    <div>
-      <Button>Click me</Button>
+    <div style={{ height: '200vh' }} className="overflow-auto">
+      <Navbar />
+      <main className="pt-20">
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+      </main>
     </div>
   );
 }
