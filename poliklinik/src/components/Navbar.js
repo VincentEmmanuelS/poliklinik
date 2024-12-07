ac1c97c7a77581442f623c25aea0dc3e9fc9a3ad
LintangKastaraErlangga <acelintara@gmail.com>
2024-12-07 19:30:28 +0700

    navbar fixes, still no logo

--- poliklinik/src/components/Navbar.js
+++ poliklinik/src/components/Navbar.js
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <nav className="flex justify-end items-center sticky top-0 h-[88px] w-full border-b-1 border-e3e3e3 box-border item-center">
+    <nav className="flex justify-end items-center sticky top-0 h-[88px] w-full border-b-1 border-e3e3e3 box-border item-center bg-[#fff]">
         <a href="/AnotherPage1" className="mx-2.5" >Lihat Dokter</a>
         <a href="/AnotherPage2" className="mx-2.5" >Pendaftaran dan Riwayat</a>
         <a href="/Login" className="mx-2.5 mr-2 px-5 p-1 bg-[#e3e3e3] rounded-lg" >Masuk</a>
